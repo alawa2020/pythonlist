6b2aca1c8fc7b8cf6df9893d76c2ff48e61aee9f
stargazing20 <huder13168@gmail.com>
2021-02-12 01:39:13 -0500

    Se agrego el loop y el print de lista

--- lista.py
+++ lista.py
@@ -1,3 +1,11 @@
 print("Bienvenido!!")
-a = "Cuantas palabras desea en su lista?"
+a = "Cuantas palabras desea en su lista?: "
 n=int(input(a))
+
+lista=[]
+for i in range(n):
+    lista.append(input("Escriba su palabra n° {}: ".format(i+1)))
+
+print()
+print("Muy bien su lista es: ")
+print(lista)
